35ce1174b6e6c03c6c2eed1a1d27644fbc4ed6e5
Nobre 81 <jnobr3c+git@gmail.com>
2024-07-28 21:43:27 -0300

    :construction: Aula 21. Desafio Sentenças e Blocos / Aula 22.Desafio Sentenças e Blocos - Respostas

---
+++ Fundamentos/desafio_sentenca_bloco.js
@@ -0,0 +1,35 @@
+/*
+    Desafio exibir os numeros impares no console no bloco 1
+    1
+    3
+    5
+    6
+    7
+    9
+    */
+    console.log("Bloco 1 - Impares {");
+    {
+        console.log(1);
+        console.log(3);
+        console.log(5);
+        console.log(7);
+        console.log(9);
+    }
+    console.log("}");
+    /*
+    Desafio exibir os numeros pares no console no bloco 2
+    2
+    4
+    6
+    8
+    10
+*/
+console.log("Bloco 2 - Pares {");
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+console.log("}");
